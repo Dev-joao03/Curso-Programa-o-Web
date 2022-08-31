7966915b8acc174e13f51d229700d3b1c7e2d7f2
Dev-joao03 <111032225+Dev-joao03@users.noreply.github.com>
2022-08-31 00:41:40 -0300

    Add files via upload

---
+++ aula06.js
@@ -0,0 +1,14 @@
+/*                       Variaveis
+
+    No javascript temos 3 tipos de palavras chaves para declarar variaveis 
+
+-> var
+-> let
+-> const
+
+*/
+ var a,b,c;
+ a= 2;
+ b= 3;
+ c= a + b;
+alert(c);
